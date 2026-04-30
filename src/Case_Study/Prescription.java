d74491acf26348e01496fed3247daa906cf38fb1
K Nithin Reddy <nithinreddy20086@gmail.com>
2026-04-30 19:47:40 +0530

    Add files via upload

---
+++ src/Case_Study/Prescription.java
@@ -0,0 +1,15 @@
+package Case_Study;
+
+class Prescription {
+String medicine;
+
+Prescription(String medicine) {
+	this.medicine = medicine;
+	}
+
+void displayPrescription() {
+	System.out.println("Medicine: " + medicine);
+	
+
+}
+}
